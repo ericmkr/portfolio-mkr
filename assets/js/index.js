f7715b924fa1cad59b89e1fba7d139b0002d21a7
Moukouri <VictorJoluke190701@gmail.com>
2026-07-30 17:19:24 +0100

    optimizing...

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 /* =========================
    HERO TYPING SECTION
-========================= */
+   ========================= */
 
 const roles = [ 
    "Front-End Developer",
@@ -61,7 +61,7 @@ typeEffect();
 
 /* =========================
    PROJECTS SHOWCASE
-========================= */
+   ========================= */
 
 const projectCards = [
    {
@@ -118,7 +118,7 @@ if (projectsGrid) {
 
 /* =========================
    SKILLS SHOWCASE
-========================= */
+   ========================= */
 
 
 
@@ -126,7 +126,7 @@ if (projectsGrid) {
 
 /* =========================
    TOOLS SHOWCASE
-========================= */
+   ========================= */
 
 // const track = document.querySelector(".tools-track");
 // const cards = [...track.children];
@@ -137,14 +137,14 @@ if (projectsGrid) {
 
 /* =========================
    PROJECT DATABASE
-========================= */
+   ========================= */
 
 
 /* =========================
    PREFILLED FORM
-========================= */
+   ========================= */
 
-const isDev = true;
+const isDev = false;
 
 if (isDev) {
    document.getElementById("name").value = "Tester";
@@ -154,7 +154,7 @@ if (isDev) {
 
 /* =========================
    BACK TO TOP BTN
-========================= */
+   ========================= */
 
 var backTopBtn = document.getElementById('back-to-top');
 
@@ -182,215 +182,3 @@ if (backTopBtn) {
    updateBackToTopVisibility();  
 
 };
-
-/* =========================
-   FORM SUBMIT FEEDBACK
-========================= */
-
-const form = document.querySelector("form");
-const overlay = document.getElementById("modal-overlay");
-const modal = document.getElementById("form-modal");
-const icon = document.getElementById("modal-icon");
-const title = document.getElementById("modal-title");
-const message = document.getElementById("modal-message");
-// const countdown = document.getElementById("modal-countdown");
-// const closeBtn = document.getElementById("modal-close");
-// let redirectTimer;
-
-// const DISPLAY_TIME = 11000;
-
-function openModal() {
-
-   overlay.hidden = false;
-   modal.hidden = false;
-
-   requestAnimationFrame(() => {
-
-      overlay.classList.add("show");
-      modal.classList.add("show");
-
-   });
-
-}
-
-
-
-
-// function closeModal() {
-//    overlay.classList.remove("show");
-//    modal.classList.remove("show");
- 
-   // setTimeout(()=>{
-   //    overlay.hidden = true;
-   //    modal.hidden = true;
-   // }, 
-   // 350
-   // );
-
-   // closeBtn.addEventListener("click", ()=>{
-   //    clearTimeout(redirectTimer);
-   //    closeModal();
-   // });
-// }
-
-
-
-
-
-function updateModal(state) {
-
-   switch (state) {
-
-      case "loading":
-         icon.textContent="⏳";
-         title.textContent="Sending...";
-         message.textContent="Your message is currently being sent.";
-      break;
-
-      case "success":
-         icon.textContent="✓";
-         title.textContent="Message sent successfully!";
-         message.textContent="Thank you for contacting me. You will be redirected shortly.";
-      break;
-
-      case "error":
-         icon.textContent="⚠";
-         title.textContent="An error occurred.";
-         message.textContent="Your message could not be sent. Please try again later.";
-      break;
-
-   };
-
-}
-
-
-
-
-
-form.addEventListener("submit", async (e) => {
-
-   e.preventDefault();
-
-   // submit-btn.disabled = true;
-   // submit-btn.textContent = "Sending...";
-
-   openModal();
-
-   updateModal("loading");
-
-   try {
-
-      const response = await fetch(form.action, {
-         method:"POST",
-         headers: {Accept: "application/json"},
-         body:new FormData(form)
-         
-      });
-
-      await debugResponse(response);
-
-      if (response.ok) {
-
-         updateModal("success");
-      
-      } 
-
-      else if (response.status === 429) {
-
-         alert (
-            "⚠️ Error 429 - Too Many Requests\n\n" +
-            "You have sent too many requests in a short period of time.\n\n" +
-            "Please wait a few minutes before trying again."
-         )
-      
-      }
-
-      else {
-
-         // alert (
-         //    `Erreur HTTP ${response.status}\n\n${response.statusText}`
-         // );
-
-         updateModal("error");
-      }
-   }
-
-   catch(error) {
-
-      console.error(error);
-
-      // alert (
-      //    "Une erreur réseau est survenue.\n\n" + error.message
-      // );
-
-      // updateModal("error");
-
-   }
-
-   // submit-btn.disabled = false;
-   // submit-btn.textContent = "Send message";
-
-   setTimeout(() => {
-
-      window.location.reload();
-
-   }, 
-
-   DISPLAY_TIME);
-
-});
-
-
-
-
-
-
-
-// document.addEventListener("keydown",(e)=>{
-//    if(e.key==="Escape" && modal.classList.contains("show")){
-//       clearTimeout(redirectTimer);
-//       closeModal();
-//    }
-// });
-
-// function startCountdown(seconds){
-//    let remaining = seconds;
-
-//    countdown.textContent = `Redirecting in ${remaining} seconds...`;
-
-//    const interval = setInterval(()=>{
-//       remaining--;
-
-//       if (remaining > 1) {
-//          countdown.textContent = `Redirecting in ${remaining} seconds...`;
-//       }
-
-//       else if (remaining === 1) {
-//          countdown.textContent = "Redirecting in 1 second...";
-//       }
-
-//       else{
-//          clearInterval(interval);
-//       }
-//    }, 
-//    1000);
-// }
-
-
-
-
-
-
-// function debugResponse(response) {
-
-//    console.group("HTTP");
-
-//    console.log("Status :", response.status);
-//    console.log("Status Text :", response.statusText);
-//    console.log("OK :", response.ok);
-//    console.log("Redirected :", response.redirected);
-//    console.log("URL :", response.url);
-
-//    console.groupEnd();
-
-// }
