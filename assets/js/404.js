fc588da78b9fe58f55561f60a18499d9eeb8b425
Moukouri <VictorJoluke190701@gmail.com>
2026-08-12 02:07:40 +0100

    portfolio.html

---
+++ assets/js/404.js
@@ -0,0 +1,59 @@
+/* =========================
+   404 REDIRECTION
+========================= */
+
+// const REDIRECT_DELAY = 5;
+
+// const countdown = document.getElementById("countdown");
+// const progressBar = document.getElementById("progress-bar");
+
+// let remainingTime = REDIRECT_DELAY;
+
+
+/* =========================
+   UPDATE COUNTDOWN
+========================= */
+
+// function updateCountdown() {
+
+//     countdown.textContent = remainingTime;
+
+//     const progress =
+//         ((REDIRECT_DELAY - remainingTime) / REDIRECT_DELAY) * 100;
+
+//     progressBar.style.width = `${progress}%`;
+// }
+
+
+/* =========================
+   REDIRECT
+========================= */
+
+// function redirectToHome() {
+
+//     window.location.replace("index.html");
+// }
+
+
+/* =========================
+   TIMER
+========================= */
+
+// updateCountdown();
+
+// const timer = setInterval(() => {
+
+//     remainingTime--;
+
+//     updateCountdown();
+
+
+//     if (remainingTime <= 0) {
+
+//         clearInterval(timer);
+
+//         redirectToHome();
+
+//     }
+
+// }, 1000);
