1a27e6781738c1599a23f8cd560e63babefc5fce
Moukouri <VictorJoluke190701@gmail.com>
2026-08-25 18:57:09 +0100

    Opt and cleaning index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -60,7 +60,7 @@ const roles = [
 
    "Front-End Developer",
 
-   "UX/UI Designer"
+   "UX/UI Designer",
 
    // "Product Designer",
 
@@ -68,11 +68,19 @@ const roles = [
 
    // "Graphic Designer",
 
-   // "QA Analyst"
+   "QA Analyst"
 
 ];
 
 const typingText = document.getElementById("typing-text");
+
+const accessibleRoles = document.getElementById("accessible-roles");
+
+if (accessibleRoles) {
+
+   accessibleRoles.textContent = roles.join(", ");
+   
+}
 
 let roleIndex = 0;
 
